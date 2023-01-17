dbc135c389c723e310e18254879256867f78bfc7
SupremeMaster23 <gabydiaz2000@hotmail.com>
2023-01-17 02:53:01 -0600

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.jsx"],//** va a todos los archivos . *jsx solo toma esta extension */
+  content: ["index.html", "./src/**/*.jsx"],//** va a todos los archivos . *jsx solo toma esta extension */
   theme: {
     extend: {},
   },
